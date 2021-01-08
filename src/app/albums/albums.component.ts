8b4afd2982e2ec9fb301427d345f93d43ef0c46d
Jeremy Walters <Jeremy@jeremys-air.myfiosgateway.com>
2021-01-08 15:35:01 -0500

    Generating Albums Component

---
+++ src/app/albums/albums.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-albums',
+  templateUrl: './views/abums.html',
+  styleUrls: ['./albums.css']
+})
+export class AlbumsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { AlbumsComponent } from './albums/albums.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    AlbumsComponent,
   ],
   imports: [
     BrowserModule,
